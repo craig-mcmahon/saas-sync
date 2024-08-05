50cf3d4f53d3a7ed0d52c4d0d3498725d165888d
Craig McMahon <craig.mcmahon@sturents.com>
2024-08-05 17:34:59 +0100

    WIP of getting slack usernames

--- src/slack.rs
+++ src/slack.rs
@@ -132,10 +132,12 @@ pub struct ChatPostMessageResponse{
 // {"channel":"C123ABC456","text":"I hope the tour went well, Mr. Wonka.","attachments":[{"text":"Who wins the lifetime supply of chocolate?","fallback":"You could be telling the computer exactly what it can do with a lifetime supply of chocolate.","color":"#3AA3E3","attachment_type":"default","callback_id":"select_simple_1234","actions":[{"name":"winners_list","text":"Who should win?","type":"select","data_source":"users"}]}]}
 
 const POST_MESSAGE_URL: &str = "https://slack.com/api/chat.postMessage";
+const GET_USER_PROFILE_URL: &str = "https://slack.com/api/users.profile.get";
 
 pub async fn send_action(env: &Env, action: Action) -> ChatPostMessageResponse {
 
     let body = ChatMessage{
+        // TODO: Get channel id from config
         channel: "#test".to_string(),
         text: action.update.text,
         thread_ts: action.target.id,
@@ -194,10 +196,11 @@ pub async fn handle_webhook(webhook: EventWebhook, env: Env, _account: Account) 
     let link = get_link_from_slack_thread(&env, &webhook.event.thread_ts.clone().unwrap()).await;
 
 
+    let user = get_user(&env, &webhook.event.user).await;
     // todo: queue?
     // todo: Replace sender id with name
     console_log!("generating action");
-    let action = generate_action(&webhook, link);
+    let action = generate_action(&webhook, link, user);
 
     console_log!("Running add comment");
     let  _res = add_comment_to_card(&env, action).await;
@@ -207,7 +210,7 @@ pub async fn handle_webhook(webhook: EventWebhook, env: Env, _account: Account) 
 }
 
 
-fn generate_action(webhook: &EventWebhook, link_result: Result<Link, Error>) -> Action {
+fn generate_action(webhook: &EventWebhook, link_result: Result<Link, Error>, user: UserProfiler) -> Action {
     let mut action: ActionType;
     let mut trello_card = None;
 
@@ -221,7 +224,7 @@ fn generate_action(webhook: &EventWebhook, link_result: Result<Link, Error>) -> 
         Some(_) => {
             action = ActionType::UpdateThread;
             ActionUpdate{
-                text: format!("{} posted in slack\n{}", webhook.event.user, webhook.event.text),
+                text: format!("{} posted in slack\n{}", user.display_name, webhook.event.text),
             }
         }
     };
@@ -278,20 +281,55 @@ fn create_action_target(_webhook: &EventWebhook, id: Option<String>) -> ActionTa
 }
 
 
+#[derive(Deserialize, PartialEq)]
+struct UserProfiler{
+    display_name: String,
+}
+
+async fn get_user(env: &Env, user_id: &str) -> UserProfiler {
+    let res = send_get_user_request(env, user_id).await;
+    let json: UserProfiler = res.json().await.unwrap();
+    return json;
+}
+#[derive(Serialize)]
+struct UserRequest {
+    user: String
+}
+async fn send_get_user_request(env: &Env, user_id: &str) -> reqwest::Response {
+    console_log!("getting users");
+    let req = UserRequest{user: user_id.to_string()};
+    let client = reqwest::Client::new();
+    let res = match client.post(GET_USER_PROFILE_URL)
+        .header("Content-Type", "application/x-www-form-urlencoded; charset=utf-8")
+        .header("Authorization", format!("Bearer {}", env.secret("SLACK_AUTH_TOKEN".as_ref()).unwrap().to_string()))
+        .form(&req)
+        .send()
+        .await {
+        Ok(value) => value,
+        Err(_err) => {
+            console_log!("{}", _err.to_string());
+            panic!("ERR");
+        },
+    };
+
+    return res;
+}
+
 mod tests {
     use std::fs;
     use worker::Error;
     use crate::action::ActionType;
     use crate::database::Link;
-    use crate::slack::EventWebhook;
+    use crate::slack::{EventWebhook, UserProfiler};
 
     #[test]
     fn generate_action_new_thread() {
         let data = fs::read_to_string("./data/slack/new-thread.json").expect("Error reading file");
 
         let webhook: EventWebhook = serde_json::from_str(&data).expect("Error parsing json");
+        let user = UserProfiler{display_name: "test".to_string()};
 
-        let action = crate::slack::generate_action(&webhook, Err(Error::RustError("test".to_string())));
+        let action = crate::slack::generate_action(&webhook, Err(Error::RustError("test".to_string())), user);
         assert_eq!(None, action.source.id);
         assert!(matches!(action.action, ActionType::None));
     }
@@ -301,8 +339,8 @@ mod tests {
         let data = fs::read_to_string("./data/slack/unknown-thread-reply.json").expect("Error reading file");
 
         let webhook: EventWebhook = serde_json::from_str(&data).expect("Error parsing json");
-
-        let action = crate::slack::generate_action(&webhook, Err(Error::RustError("test".to_string())));
+        let user = UserProfiler{display_name: "test".to_string()};
+        let action = crate::slack::generate_action(&webhook, Err(Error::RustError("test".to_string())), user);
         assert_eq!(Some("1715524581.123456".to_string()), action.source.id);
         assert!(matches!(action.action, ActionType::None));
     }
@@ -316,7 +354,9 @@ mod tests {
             slack_thread: "1715287188.123456".to_string(),
             trello_card: "ABCDEFG".to_string(),
         };
-        let action = crate::slack::generate_action(&webhook, Ok(link));
+        let user = UserProfiler{display_name: "test".to_string()};
+
+        let action = crate::slack::generate_action(&webhook, Ok(link), user);
         assert_eq!(Some("1715287188.123456".to_string()), action.source.id);
         assert!(matches!(action.action, ActionType::UpdateThread));
     }
@@ -326,8 +366,9 @@ mod tests {
         let data = fs::read_to_string("./data/slack/thread-replied-bot.json").expect("Error reading file");
 
         let webhook: EventWebhook = serde_json::from_str(&data).expect("Error parsing json");
+        let user = UserProfiler{display_name: "test".to_string()};
 
-        let action = crate::slack::generate_action(&webhook, Err(Error::RustError("test".to_string())));
+        let action = crate::slack::generate_action(&webhook, Err(Error::RustError("test".to_string())), user);
         assert_eq!(Some("1715287188.123456".to_string()), action.source.id);
         assert!(matches!(action.action, ActionType::None));
     }
